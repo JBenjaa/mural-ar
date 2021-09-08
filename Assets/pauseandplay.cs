053302629ba2550fc97ee9b3c9060509e87f702b
JBenjaa <83830575+JBenjaa@users.noreply.github.com>
2021-09-08 18:30:06 -0400

    modificacion del video y programacion del panel equipo.

--- Assets/pauseandplay.cs
+++ Assets/pauseandplay.cs
@@ -5,6 +5,9 @@ using UnityEngine.Video;
 
 public class pauseandplay : MonoBehaviour
 {
+	
+	// Start is called on the frame when a script is enabled just before any of the Update methods is called the first time.
+
 	public VideoPlayer video;
 	
 	public void play(){
